9bbccf6d75fc4b61e835d90bed61073f4cdd5d2a
RISHIK BODA <rishikboda@gmail.com>
2026-08-03 22:13:07 +0530

    made some changes

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -1,1 +1,9 @@
 print("hello world!")
+# to make some change in your file and want to upload it the changed file in github
+#follow these commands
+#git add .
+#git status
+#git commit -m "made some changes"
+#git push
+
+#and now save the file in vs ccode and do the changes in the git hub
