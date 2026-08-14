f761b590e65243e0408208c25df4d21638152032
RISHIK BODA <rishikboda@gmail.com>
2026-08-14 22:34:59 +0530

    updated

--- python-for-ai/bascis-of-python/data structure parterns/patern 3.py
+++ python-for-ai/bascis-of-python/data structure parterns/patern 3.py
@@ -1,11 +1,11 @@
-def patern2(n):
+def patern(n):
     for i in range(n):
-        for j in range(-1, i):
-            print("*", end=" ")
+        for j in range(1, i + 2):
+            print(j, end=" ")
         print(" ")
 
 
 t = int(input(" enter the parterns you want"))
 for i in range(t):
     n = int(input("enter the partern size n*n"))
-    patern2(n)
+    patern(n)
