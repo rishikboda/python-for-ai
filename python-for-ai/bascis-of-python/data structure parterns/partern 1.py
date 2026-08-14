6610e3ced5200a343a9516b0807a8c0ec6dbab96
RISHIK BODA <rishikboda@gmail.com>
2026-08-14 12:19:20 +0530

    for loop parterns using functions

---
+++ python-for-ai/bascis-of-python/data structure parterns/partern 1.py
@@ -0,0 +1,9 @@
+def partern(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end=" ")
+        print(" ")
+t= int(input(" enter the parterns you want"))
+for i in range(0,t):
+    n=int(input("enter the partern size n*n"))
+    partern(n)
