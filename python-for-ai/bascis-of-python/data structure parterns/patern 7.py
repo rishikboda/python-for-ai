18d4a8dd0c67ea33a0b108d00b06039a1842549f
RISHIK BODA <rishikboda@gmail.com>
2026-08-15 22:58:00 +0530

    initially

---
+++ python-for-ai/bascis-of-python/data structure parterns/patern 7.py
@@ -0,0 +1,12 @@
+def patern7(n):
+    for i in range(0, n):
+        for j in range(0, n - i - 1):
+            print(" ", end=" ")
+        for j in range(0, 2 * i + 1):
+            print("*", end=" ")
+        for j in range(0, n - i - 1):
+            print(" ", end=" ")
+        print(" ")
+
+
+patern7(5)
